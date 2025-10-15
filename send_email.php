b9e0726c73f5bd17818865f9a29d322408ec078e
Muhammad Arifin <ngaripin6@gmail.com>
2025-10-15 09:46:26 +0700

    add php mailer

--- send_email.php
+++ send_email.php
@@ -1,40 +1,48 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Alamat email tujuan (email kamu)
-    $to = "ngaripin@gmail.com";
+    $mail = new PHPMailer(true);
 
-    // Subjek email
-    $email_subject = "New Message from $name: $subject";
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'ngaripin6@gmail.com';
+        $mail->Password   = 'kwuhclqyjupsrdjh'; // tanpa spasi
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+        $mail->SMTPDebug  = 2; // Tambahkan untuk debug (sementara)
 
-    // Isi pesan email
-    $email_body = "
-    You have received a new message from your portfolio contact form.\n\n
-    Name: $name\n
-    Email: $email\n
-    Subject: $subject\n
-    Message:\n$message
-    ";
 
-    // Header email
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
+        // Pengirim dan penerima
+        $mail->setFrom('ngaripin6@gmail.com', 'Portfolio Website');
+        $mail->addAddress('ngaripin@gmail.com'); // Email penerima (kamu sendiri)
+        $mail->addReplyTo($email, $name);
 
-    // Kirim email
-    if (mail($to, $email_subject, $email_body, $headers)) {
-        echo "<script>
-            alert('Message sent successfully!');
-            window.location.href='index.html';
-        </script>";
-    } else {
-        echo "<script>
-            alert('Failed to send message. Please try again later.');
-            window.history.back();
-        </script>";
+        // Konten email
+        $mail->isHTML(true);
+        $mail->Subject = "Message from $name: $subject";
+        $mail->Body    = "
+            <h3>New Message from Portfolio</h3>
+            <p><b>Name:</b> $name</p>
+            <p><b>Email:</b> $email</p>
+            <p><b>Subject:</b> $subject</p>
+            <p><b>Message:</b><br>$message</p>
+        ";
+
+        $mail->send();
+        echo "success";
+    } catch (Exception $e) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
     }
 }
 ?>
